7fe28e0c7770f3919151b621d44a10fb8725a85b
Profa.Vanessa <162433360+vrocha-profa@users.noreply.github.com>
2026-01-02 15:54:50 -0300

    module no supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,10 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  const SUPABASE_URL = "https://votltdaxhvcvnbejhqzp.supabase.co";
-  const SUPABASE_ANON_KEY = "sb_publishable_mOUXhFSIWoKEt1LGq1F4vQ_wDAoMeB1";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-  window.supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-</script>
+const SUPABASE_URL = "https://votltdaxhvcvnbejhqzp.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_mOUXhFSIWoKEt1LGq1F4vQ_wDAoMeB1";
+
+window.supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
