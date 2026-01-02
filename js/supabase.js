59e9f75d7483e79cba49f99416ed7ff6fca10bb9
Profa.Vanessa <162433360+vrocha-profa@users.noreply.github.com>
2026-01-02 15:50:29 -0300

    api key

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 <script>
   const SUPABASE_URL = "https://votltdaxhvcvnbejhqzp.supabase.co";
-  const SUPABASE_ANON_KEY = process.env.SUPABASE_KEY;
+  const SUPABASE_ANON_KEY = "sb_publishable_mOUXhFSIWoKEt1LGq1F4vQ_wDAoMeB1";
 
   window.supabase = window.supabase.createClient(
     SUPABASE_URL,
