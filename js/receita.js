7eb98be0f8f95393646510782390c86f4ef10f7b
Profa.Vanessa <162433360+vrocha-profa@users.noreply.github.com>
2026-01-10 07:23:23 -0300

    aparecer receitas

--- js/receita.js
+++ js/receita.js
@@ -9,6 +9,10 @@ async function carregar() {
     .eq("id", id)
     .single();
 
+console.log("ID da URL:", id);
+console.log("Receita do Supabase:", receita);
+
+
   if (error) {
     alert("Erro ao carregar receita");
     return;
