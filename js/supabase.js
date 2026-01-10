dd613859da09a07d6d63bca8384eaccafadf0d70
Profa.Vanessa <162433360+vrocha-profa@users.noreply.github.com>
2026-01-10 11:59:45 -0300

    arrumar supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://votltdaxhvcvnbejhqzp.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_mOUXhFSIWoKEt1LGq1F4vQ_wDAoMeB1";
-
-window.supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+export const supabase = createClient(
+  "https://votltdaxhvcvnbejhqzp.supabase.co",
+  "sb_publishable_mOUXhFSIWoKEt1LGq1F4vQ_wDAoMeB1"
 );
 
